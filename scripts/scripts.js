71928169128167bcfce2609c966477e862d75b69
PeterJMac <peter_macdonald@live.com>
2019-09-28 13:50:42 +0200

    Fix Slider added Composer
    Fixed slider correctly displaying the right amount of characters in the password.
    
    Added composer.json

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ function generate(){
     let password = "";
 
     //Create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
+    for(var i = 1; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
